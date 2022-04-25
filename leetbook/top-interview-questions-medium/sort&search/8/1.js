08a2085690def1d0e2fe87f04d0ff02f81c989a8
erlangera <1638442068@qq.com>
2022-04-26 00:18:01 +0800

    section sort&search

--- leetbook/top-interview-questions-medium/sort&search/8/1.js
+++ leetbook/top-interview-questions-medium/sort&search/8/1.js
@@ -8,13 +8,14 @@ var searchMatrix = function (matrix, target) {
 };
 
 function search(matrix, target, x1, y1, x2, y2) {
+  if (x1 > x2 || y1 > y2) return false;
   if (x1 === x2 && y1 === y2) return matrix[x1][y1] === target;
 
   let sx = x1,
     sy = y1,
     ex = x2,
     ey = y2;
-  while (sx < ex && sy < ey) {
+  while (sx < ex || sy < ey) {
     const x = Math.ceil((sx + ex) / 2);
     const y = Math.ceil((sy + ey) / 2);
     if (matrix[x][y] < target) {
@@ -25,15 +26,9 @@ function search(matrix, target, x1, y1, x2, y2) {
         if (matrix[sx][sy] === target) {
           return true;
         }
-        if (sx === ex) {
-          return (
-            (y !== y1 && search(matrix, target, x, y1, x2, y - 1)) ||
-            (x !== x1 && search(matrix, target, x1, y, x - 1, y2))
-          );
-        }
         return (
-          search(matrix, target, x, y1, x2, y - 1) ||
-          search(matrix, target, x1, y - 1, x, y2)
+          search(matrix, target, sx === x ? x + 1 : x, y1, x2, y - 1) ||
+          search(matrix, target, x1, sy === y ? y + 1 : y, x - 1, y2)
         );
       } else {
         ex = x;
