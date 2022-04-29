c5d46e8c72e0632fd740356b0aae5bd9b1831447
erlangera <1638442068@qq.com>
2022-04-29 23:19:30 +0800

    section array&string

---
+++ leetbook/top-interview-questions-hard/array&string/6/1.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function (nums) {
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] > 0 && nums[i] <= nums.length) {
+      while (
+        nums[i] !== i + 1 &&
+        nums[i] > 0 &&
+        nums[i] <= nums.length &&
+        nums[nums[i] - 1] !== nums[i]
+      ) {
+        const temp = nums[nums[i] - 1];
+        nums[nums[i] - 1] = nums[i];
+        nums[i] = temp;
+      }
+    }
+  }
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+  return nums.length + 1;
+};
